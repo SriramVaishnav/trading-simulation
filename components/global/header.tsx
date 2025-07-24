c0ffcf207f5c4a7495b543b191c6487cec220064
Sriram Vaishnav <sriramvaishnavr@gmail.com>
2025-07-24 07:43:31 +0530

    rm: unnecessary shadcn comps

--- components/global/header.tsx
+++ components/global/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-full p-6 border-b flex justify-between items-center">
+    <header className="w-full p-6 border-b-2 flex justify-between items-center sticky top-0 left-0 bg-white z-50">
       <PredLogoSvg />
       <div className="flex items-center gap-x-4">
         <StarSvg />
